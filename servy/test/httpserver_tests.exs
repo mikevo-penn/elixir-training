d30ab8c2b77ed4a8f2ae516563b346e2eadad802
Mike Vo <mike.vo@penn-interactive.com>
2022-07-26 21:46:24 -0400

    Completed chapter 22. basic sending and receiving messages between forked processes.

--- servy/test/httpserver_tests.exs
+++ servy/test/httpserver_tests.exs
@@ -3,32 +3,44 @@ defmodule HttpServerTests do
   alias Servy.HttpServer
   alias Servy.HttpClient
 
+  # Globally spawn the expected http server on port 4000
+  pid = spawn(HttpServer, :start, [4000])
+
+  test "requests and responses via HttPoison" do
+    {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers, Dookie, Nöödle, and Mookie"
+  end
+
+  test "multiple concurrent requests" do
+    parent = self()
+
+    max_concurrent_requests = 5
+
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+        send(parent, {:okzz, response})
+      end)
+    end
+
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:okzz, response} ->
+        assert response.status_code == 200
+        assert response.body == "Bears, Lions, Tigers, Dookie, Nöödle, and Mookie"
+      end
+    end
+  end
+
   test "accepts a request on a socket and sends back a response" do
-    pid = spawn(HttpServer, :start, [4000])
+    {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
 
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
-
-    response = HttpClient.send_request(request)
-    Process.exit(pid, :kill)
-
-    assert response == """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/html\r
-    Content-Length: 50\r
-    \r
-    Bears, Lions, Tigers, Dookie, Nöödle, and Mookie
-    """
+    assert response.body == "Bears, Lions, Tigers, Dookie, Nöödle, and Mookie"
   end
 
   test "Test the /about via spawned process." do
-    pid = spawn(HttpServer, :start, [4000])
-
     request = """
     GET /about HTTP/1.1\r
     Host: example.com\r
@@ -38,7 +50,6 @@ defmodule HttpServerTests do
     """
 
     response = HttpClient.send_request(request)
-    Process.exit(pid, :kill)
 
     assert response == """
     HTTP/1.1 200 OK\r
